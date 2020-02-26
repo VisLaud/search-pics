992fe9f60eadbb8551167c947d0cb6aaff9e08e6
Simanta Thapa <44049186+VisLaud@users.noreply.github.com>
2020-02-26 13:14:07 -0800

    started the axios request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID DT97VCmrOJy5IckTn3DCPxJoI31qtEW94ZjZ8JqYIFc"
+      }
+    });
   }
   render() {
     return (
